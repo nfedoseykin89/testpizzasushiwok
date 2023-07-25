00c3dd3a530a396927def85e4bc43be0732ccfc0
aktarus-dev <Aktarus.Developer@itbrain.ru>
2023-07-25 08:30:14 +0300

    Add copyright

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer">
         <div class="container">
-
+            <p>© Fedoseykin Nikolay 2023. PHP developer / Test task example.</p>
         </div>
     </div>
 </footer>
